76aecd00a9a12b367c00f595bd259412da6c4199
saraduponchelle-wq <saraduponchelle@gmail.com>
2026-01-10 03:17:20 +0000

    dwa

--- comands/punition.py
+++ comands/punition.py
@@ -101,7 +101,7 @@ async def punition(interaction: discord.Interaction, usuario: discord.Member):
         )
 
         # Silenciar sin volver a enviar embed al terminar
-        await silenciar_en_canal(canal, usuario, duracion, stop_embed=False)
+        await silenciar_en_canal(canal, usuario, duracion, stop_embed=True)
 
 
 
